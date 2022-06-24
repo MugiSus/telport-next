e3a427671794655563bce27772fc2257b03e407d
MugiSus <mmgiss256@gmail.com>
2022-06-24 21:08:23 +0900

    Add _offline.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
+const nextConfig = {
     pwa: {
         dest: "public",
         register: true,
         skipWaiting: true,
         disable: process.env.NODE_ENV === "development",
     },
-    reactStrictMode: true,
-});
+    reactStrictMode: false,
+}
+
+module.exports = withPWA(nextConfig);
