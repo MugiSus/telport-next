cf1f8a22b8c8d69bad0431d76e72d828d88d73e9
MugiSus <mmgiss256@gmail.com>
2022-06-25 15:48:26 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,7 +11,11 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                
+                <div class={styles.callVisualizer} id="call-visual">
+                    <div className={styles.callVisualizerGrid}>
+                        
+                    </div>
+                </div>
             </main>
         </div>
     )
