06e53ce96115cb3232ccb3f2f1cddaaf6f50d774
MugiSus <mmgiss256@gmail.com>
2022-06-28 00:31:25 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-// import Window from '../components/Window'
 
 export default function Home() {
     return (
@@ -12,21 +11,21 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <div className={`${styles.window} ${styles.listenerVisualizer}`} id="listner-visual">
+                <div className={`${styles.window} ${styles.listenerVisualizer}`} id="caller-visual">
                     <div className={styles.visualizerGrid}>
                         
                     </div>
                 </div>
-                <div className={`${styles.window} ${styles.listener}`} id="listner">
+                <div className={`${styles.window} ${styles.listener}`} id="caller">
                     
                 </div>
                 <div className={`${styles.window} ${styles.home}`} id="home">
                     
                 </div>
-                <div className={`${styles.window} ${styles.caller}`} id="caller">
+                <div className={`${styles.window} ${styles.caller}`} id="listener">
                     
                 </div>
-                <div className={`${styles.window} ${styles.callerVisualizer}`} id="caller-visual">
+                <div className={`${styles.window} ${styles.callerVisualizer}`} id="listener-visual">
                     <div className={styles.visualizerGrid}>
                         
                     </div>
