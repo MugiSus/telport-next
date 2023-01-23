ae0c2ef6fdfa56d0747c25e5dc543904821122f3
MugiSus <mmgiss256@gmail.com>
2023-01-23 17:31:32 +0900

    tabify indent

--- components/sources/telport.js
+++ components/sources/telport.js
@@ -1,158 +1,158 @@
 const oscillator = class {
-    constructor(audioContext, frequency) {
-        this.audioContext = audioContext;
-        this.frequency = frequency;
-        this.started = false;
+	constructor(audioContext, frequency) {
+		this.audioContext = audioContext;
+		this.frequency = frequency;
+		this.started = false;
 
-        this.oscillator = this.audioContext.createOscillator();
-        this.oscillator.frequency.value = this.frequency;
-        this.oscillator.type = "sine";
+		this.oscillator = this.audioContext.createOscillator();
+		this.oscillator.frequency.value = this.frequency;
+		this.oscillator.type = "sine";
 
-        this.gainNode = this.audioContext.createGain();
-        this.gainNode.gain.value = 0;
-        this.gainNode.connect(this.audioContext.destination);
+		this.gainNode = this.audioContext.createGain();
+		this.gainNode.gain.value = 0;
+		this.gainNode.connect(this.audioContext.destination);
 
-        this.oscillator.connect(this.gainNode);
-    }
+		this.oscillator.connect(this.gainNode);
+	}
 
-    start(length) {
-        if (!this.started)
-            this.oscillator.start(this.audioContext.currentTime);
-        
-        this.started = true;
-        this.gainNode.gain.value = 0.05;
-        this.gainNode.gain.setValueAtTime(0, this.audioContext.currentTime + length - 0.02);
-    }
+	start(length) {
+		if (!this.started)
+			this.oscillator.start(this.audioContext.currentTime);
+		
+		this.started = true;
+		this.gainNode.gain.value = 0.05;
+		this.gainNode.gain.setValueAtTime(0, this.audioContext.currentTime + length - 0.02);
+	}
 }
 
 const Telport = class {
-    constructor({ fftSize, start, amount, step = 2, interval = 100 }) {
-        if (amount % 8 > 0) {
-            throw new Error("[TELPort] The amount must be a multiple of 8");
-        }
+	constructor({ fftSize, start, amount, step = 2, interval = 100 }) {
+		if (amount % 8 > 0) {
+			throw new Error("[TELPort] The amount must be a multiple of 8");
+		}
 
-        this.fftSize = fftSize;
-        this.start = start;
-        this.amount = amount;
-        this.step = step;
-        this.size = amount / 8;
-        this.interval = interval;
+		this.fftSize = fftSize;
+		this.start = start;
+		this.amount = amount;
+		this.step = step;
+		this.size = amount / 8;
+		this.interval = interval;
 
-        this.callInterval = null;
-        this.recieveInterval = null;
+		this.callInterval = null;
+		this.recieveInterval = null;
 
-        this.hertzPerBin = 44100 / this.fftSize;
-        this.frequencies = new Uint16Array(
-            [...new Array(this.amount).keys()].map(i => (this.start + i * this.step) * this.hertzPerBin)
-        );
+		this.hertzPerBin = 44100 / this.fftSize;
+		this.frequencies = new Uint16Array(
+			[...new Array(this.amount).keys()].map(i => (this.start + i * this.step) * this.hertzPerBin)
+		);
 
-        if (typeof window === "undefined") return; // don't do anything if we're not in the browser
+		if (typeof window === "undefined") return; // don't do anything if we're not in the browser
 
-        this.audioContext = new (AudioContext ?? webkitAudioContext)();
-        this.oscillators = Array.from(this.frequencies).map(frequency => new oscillator(this.audioContext, frequency));
-    }
+		this.audioContext = new (AudioContext ?? webkitAudioContext)();
+		this.oscillators = Array.from(this.frequencies).map(frequency => new oscillator(this.audioContext, frequency));
+	}
 
-    callSingle(uint8Array, length) {
-        return new Promise((resolve, reject) => {
-            if (typeof window === "undefined")
-                resolve(); // don't do anything if we're not in the browser
+	callSingle(uint8Array, length) {
+		return new Promise((resolve, reject) => {
+			if (typeof window === "undefined")
+				resolve(); // don't do anything if we're not in the browser
 
-            this.oscillators.forEach((oscillator, index) => {
-                const bit = (uint8Array[Math.trunc(index / 8)] >> (index % 8)) & 1;
-                if (bit === 1) 
-                    oscillator.start(length / 1000);
-            });
+			this.oscillators.forEach((oscillator, index) => {
+				const bit = (uint8Array[Math.trunc(index / 8)] >> (index % 8)) & 1;
+				if (bit === 1) 
+					oscillator.start(length / 1000);
+			});
 
-            window.setTimeout(resolve, length);
-        })
-    }
+			window.setTimeout(resolve, length);
+		})
+	}
 
-    callTuner(length = 10000) {
-        this.clearCall();
-        
-        return new Promise((resolve, reject) => {
-            if (typeof window === "undefined")
-                resolve(); // don't do anything if we're not in the browser
-            
-            this.callSingle(new Uint8Array(this.size).fill(0b11001100), length).then(resolve);
-        })
-    }
-    
-    callStarter(datatype, datasize, length = 500) {
-        this.clearCall();
-        
-        return new Promise((resolve, reject) => {
-            if (typeof window === "undefined")
-                resolve(); // don't do anything if we're not in the browser
-            
-            const uint8Array = new Uint8Array([
-                0xAA, 0xAA, 0xAA, 0xAA,
-                datasize,
-                datasize >> 8,
-                datasize >> 16,
-                datasize >> 24,
-                datatype,
-            ]);
-            console.log(uint8Array);
-            this.callSingle(uint8Array, length).then(resolve);
-        });
-    }
+	callTuner(length = 10000) {
+		this.clearCall();
+		
+		return new Promise((resolve, reject) => {
+			if (typeof window === "undefined")
+				resolve(); // don't do anything if we're not in the browser
+			
+			this.callSingle(new Uint8Array(this.size).fill(0b11001100), length).then(resolve);
+		})
+	}
+	
+	callStarter(datatype, datasize, length = 500) {
+		this.clearCall();
+		
+		return new Promise((resolve, reject) => {
+			if (typeof window === "undefined")
+				resolve(); // don't do anything if we're not in the browser
+			
+			const uint8Array = new Uint8Array([
+				0xAA, 0xAA, 0xAA, 0xAA,
+				datasize,
+				datasize >> 8,
+				datasize >> 16,
+				datasize >> 24,
+				datatype,
+			]);
+			console.log(uint8Array);
+			this.callSingle(uint8Array, length).then(resolve);
+		});
+	}
+	
+	call(uint8Array) {
+		this.clearCall();
 
-    call(uint8Array) {
-        this.clearCall();
+		return new Promise((resolve, reject) => {
+			if (typeof window === "undefined")
+				resolve(); // don't do anything if we're not in the browser
+		
+			if (this.oscillators === null)
+				reject(new Error("[TELPort] The TELPort has not been initialized yet"));
+		
+			// const dataView = new DataView(arrayBuffer);
+			const dataLength = uint8Array.length;
+			const data = uint8Array;
+		
+			let offset = this.size;
+			this.callSingle(data.slice(0, this.size), this.interval);
 
-        return new Promise((resolve, reject) => {
-            if (typeof window === "undefined")
-                resolve(); // don't do anything if we're not in the browser
-        
-            if (this.oscillators === null)
-                reject(new Error("[TELPort] The TELPort has not been initialized yet"));
-        
-            // const dataView = new DataView(arrayBuffer);
-            const dataLength = uint8Array.length;
-            const data = uint8Array;
-        
-            let offset = this.size;
-            this.callSingle(data.slice(0, this.size), this.interval);
+			this.callInterval = window.setInterval(() => {
+				this.callSingle(data.slice(offset, offset + this.size), this.interval);
 
-            this.callInterval = window.setInterval(() => {
-                this.callSingle(data.slice(offset, offset + this.size), this.interval);
+				offset += this.size;
+				if (offset > dataLength) {
+					resolve();
+					window.clearInterval(this.callInterval);
+				}
+			}, this.interval);
+		})
+	}
 
-                offset += this.size;
-                if (offset > dataLength) {
-                    resolve();
-                    window.clearInterval(this.callInterval);
-                }
-            }, this.interval);
-        })
-    }
+	callText(text) {
+		return new Promise((resolve, reject) => {
+			if (typeof window === "undefined")
+				resolve(); // don't do anything if we're not in the browser
 
-    callText(text) {
-        return new Promise((resolve, reject) => {
-            if (typeof window === "undefined")
-                resolve(); // don't do anything if we're not in the browser
+			const encoder = new TextEncoder();
+			const uint8Array = encoder.encode(text);
+	
+			this.callStarter(0, uint8Array.length, 500).then(() => this.call(uint8Array));
+		});
+	}
 
-            const encoder = new TextEncoder();
-            const uint8Array = encoder.encode(text);
-    
-            this.callStarter(0, uint8Array.length, 500).then(() => this.call(uint8Array));
-        });
-    }
+	clearCall() {
+		if (this.callInterval !== null)
+			window.clearInterval(this.callInterval);
 
-    clearCall() {
-        if (this.callInterval !== null)
-            window.clearInterval(this.callInterval);
+		this.oscillators.forEach(oscillator => oscillator.gainNode.gain.value = 0);
+	}
 
-        this.oscillators.forEach(oscillator => oscillator.gainNode.gain.value = 0);
-    }
+	recieveTuner() {
+		this.analyser = this.audioContext.createAnalyser();
+	}
 
-    recieveTuner() {
-        
-    }
-
-    enableReceiver() {
-        
-    }
+	enableReceiver() {
+		
+	}
 }
 
 export default Telport;
--- pages/index.js
+++ pages/index.js
@@ -9,120 +9,120 @@ import { scrollIntoViewById, scrollIntoThis } from '../components/sources/scroll
 import Telport from '../components/sources/telport'
 
 export default function Home() {
-    const telport = new Telport({
-        fftSize: 1024, 
-        start: 24,
-        amount: 40,
-        step: 2,
-        interval: 100
-    });
-    
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            scrollIntoViewById("home", { behavior: "auto" });
-        }
-    }, []);
-    
-    const visualizerItemsHtml = Array.from(telport.frequencies).map((frequency, index) => (
-        <div key={index} className={`${styles.visualizerItem} ${frequency >= 10000 ? styles.condensed : ""}`}>
-            {frequency}
-        </div>
-    ))
+	const telport = new Telport({
+		fftSize: 1024, 
+		start: 24,
+		amount: 40,
+		step: 2,
+		interval: 100
+	});
+	
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			scrollIntoViewById("home", { behavior: "auto" });
+		}
+	}, []);
+	
+	const visualizerItemsHtml = Array.from(telport.frequencies).map((frequency, index) => (
+		<div key={index} className={`${styles.visualizerItem} ${frequency >= 10000 ? styles.condensed : ""}`}>
+			{frequency}
+		</div>
+	))
 
-    const callText = () => {
-        const text = document.querySelector(`.${styles.caller} .${styles.textarea}`).value;
+	const callText = () => {
+		const text = document.querySelector(`.${styles.caller} .${styles.textarea}`).value;
 
-        telport.callText(text).then(() => {
-            console.log("request sent.");
-        });
-    }
+		telport.callText(text).then(() => {
+			console.log("request sent.");
+		});
+	}
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>TELPort Next</title>
-                <meta name="description" content="Next TELPort is powered by Next" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>TELPort Next</title>
+				<meta name="description" content="Next TELPort is powered by Next" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-            <main className={styles.main}>
-                <div className={`${styles.window} ${styles.callerVisualizer}`} id="caller-visualizer" onClick={scrollIntoThis}>
-                    <WindowArrow htmlFor="caller" />
+			<main className={styles.main}>
+				<div className={`${styles.window} ${styles.callerVisualizer}`} id="caller-visualizer" onClick={scrollIntoThis}>
+					<WindowArrow htmlFor="caller" />
 
-                    <div className={styles.visualizerGrid}>
-                        {visualizerItemsHtml}
-                    </div>
-                </div>
+					<div className={styles.visualizerGrid}>
+						{visualizerItemsHtml}
+					</div>
+				</div>
 
-                <div className={`${styles.window} ${styles.caller}`} id="caller" onClick={scrollIntoThis}>
-                    <WindowArrow htmlFor="caller-visualizer" isLeftward={true} />
-                    <WindowArrow htmlFor="home" />
+				<div className={`${styles.window} ${styles.caller}`} id="caller" onClick={scrollIntoThis}>
+					<WindowArrow htmlFor="caller-visualizer" isLeftward={true} />
+					<WindowArrow htmlFor="home" />
 
-                    <img className={styles.modeSelector} src={"./svg/caller-button-modeselector-text.svg"} alt="caller-button-modeselector" />
-                    <textarea className={styles.textarea} placeholder="Type something you want to call..." />
-                    <div className={styles.buttonsGroup}>
-                        <div className={styles.buttonContainer} onClick={() => telport.callTuner()}>
-                            <img className={styles.buttonLabel} src={"./svg/caller-label-tuning.svg"} alt="caller-label-tuning" />
-                            <img className={styles.button} src={"./svg/caller-button-tuning.svg"} alt="caller-button-tuning" />
-                        </div>
-                        <div className={styles.buttonContainer} onClick={callText}>
-                            <img className={styles.buttonLabel} src={"./svg/caller-label-call.svg"} alt="caller-label-call" />
-                            <img className={styles.button} src={"./svg/caller-button-call.svg"} alt="caller-button-call" />
-                        </div>
-                    </div>
-                </div>
+					<img className={styles.modeSelector} src={"./svg/caller-button-modeselector-text.svg"} alt="caller-button-modeselector" />
+					<textarea className={styles.textarea} placeholder="Type something you want to call..." />
+					<div className={styles.buttonsGroup}>
+						<div className={styles.buttonContainer} onClick={() => telport.callTuner()}>
+							<img className={styles.buttonLabel} src={"./svg/caller-label-tuning.svg"} alt="caller-label-tuning" />
+							<img className={styles.button} src={"./svg/caller-button-tuning.svg"} alt="caller-button-tuning" />
+						</div>
+						<div className={styles.buttonContainer} onClick={callText}>
+							<img className={styles.buttonLabel} src={"./svg/caller-label-call.svg"} alt="caller-label-call" />
+							<img className={styles.button} src={"./svg/caller-button-call.svg"} alt="caller-button-call" />
+						</div>
+					</div>
+				</div>
 
-                <div className={`${styles.window} ${styles.home}`} id="home">
-                    <div className={styles.callerLink} onClick={() => scrollIntoViewById("caller")}>
-                        <img className={styles.linkBackgroundIcon} src={`./svg/home-call-icon.svg`} alt="caller-link-background" />
-                        <img className={styles.linkTitle} src={"./svg/home-call-title-ja.svg"} alt="call-title" />
-                        <div className={styles.linkArrowContainer}>
-                            {
-                                [...Array(5).keys()].map(index => (
-                                    <img key={index} className={styles.linkArrow} src={"./svg/home-call-arrow.svg"} alt="call-arrow" />
-                                ))
-                            }
-                        </div>
-                    </div>
+				<div className={`${styles.window} ${styles.home}`} id="home">
+					<div className={styles.callerLink} onClick={() => scrollIntoViewById("caller")}>
+						<img className={styles.linkBackgroundIcon} src={`./svg/home-call-icon.svg`} alt="caller-link-background" />
+						<img className={styles.linkTitle} src={"./svg/home-call-title-ja.svg"} alt="call-title" />
+						<div className={styles.linkArrowContainer}>
+							{
+								[...Array(5).keys()].map(index => (
+									<img key={index} className={styles.linkArrow} src={"./svg/home-call-arrow.svg"} alt="call-arrow" />
+								))
+							}
+						</div>
+					</div>
 
-                    <div className={styles.listenerLink} onClick={() => scrollIntoViewById("listener")}>
-                        <img className={styles.linkBackgroundIcon} src={`./svg/home-listen-icon.svg`} alt="caller-link-background" />
-                        <img className={styles.linkTitle} src={"./svg/home-listen-title-ja.svg"} alt="listen-title" />
-                        <div className={styles.linkArrowContainer}>
-                            {
-                                [...Array(5).keys()].map(index => (
-                                    <img key={index} className={styles.linkArrow} src={"./svg/home-listen-arrow.svg"} alt="listen-arrow" />
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
+					<div className={styles.listenerLink} onClick={() => scrollIntoViewById("listener")}>
+						<img className={styles.linkBackgroundIcon} src={`./svg/home-listen-icon.svg`} alt="caller-link-background" />
+						<img className={styles.linkTitle} src={"./svg/home-listen-title-ja.svg"} alt="listen-title" />
+						<div className={styles.linkArrowContainer}>
+							{
+								[...Array(5).keys()].map(index => (
+									<img key={index} className={styles.linkArrow} src={"./svg/home-listen-arrow.svg"} alt="listen-arrow" />
+								))
+							}
+						</div>
+					</div>
+				</div>
 
-                <div className={`${styles.window} ${styles.listener}`} id="listener" onClick={scrollIntoThis}>
-                    <WindowArrow htmlFor="home" isLeftward={true} />
-                    <WindowArrow htmlFor="listener-visualizer" />
+				<div className={`${styles.window} ${styles.listener}`} id="listener" onClick={scrollIntoThis}>
+					<WindowArrow htmlFor="home" isLeftward={true} />
+					<WindowArrow htmlFor="listener-visualizer" />
 
-                    <img className={styles.modeSelector} src={"./svg/listener-button-modeselector-text.svg"} alt="listener-button-modeselector" />
-                    <textarea className={styles.textarea} placeholder="Something heard will appear over here..." readOnly/>
-                    <div className={styles.buttonsGroup}>
-                        <div className={styles.buttonContainer}>
-                            <img className={styles.buttonLabel} src={"./svg/listener-label-tuning.svg"} alt="listener-label-tuning" />
-                            <img className={styles.button} src={"./svg/listener-button-tuning.svg"} alt="listener-button-tuning" />
-                        </div>
-                        <div className={styles.buttonContainer}>
-                            <img className={styles.buttonLabel} src={"./svg/listener-label-listen.svg"} alt="listener-label-listen" />
-                            <img className={styles.button} src={"./svg/listener-button-listen.svg"} alt="listener-button-listen" />
-                        </div>
-                    </div>
-                </div>
-                
-                <div className={`${styles.window} ${styles.listenerVisualizer}`} id="listener-visualizer" onClick={scrollIntoThis}>
-                    <WindowArrow htmlFor="listener" isLeftward={true}/>
-                    <div className={styles.visualizerGrid}>
-                        {visualizerItemsHtml}
-                    </div>
-                </div>
-            </main>
-        </div>
-    )
+					<img className={styles.modeSelector} src={"./svg/listener-button-modeselector-text.svg"} alt="listener-button-modeselector" />
+					<textarea className={styles.textarea} placeholder="Something heard will appear over here..." readOnly/>
+					<div className={styles.buttonsGroup}>
+						<div className={styles.buttonContainer}>
+							<img className={styles.buttonLabel} src={"./svg/listener-label-tuning.svg"} alt="listener-label-tuning" />
+							<img className={styles.button} src={"./svg/listener-button-tuning.svg"} alt="listener-button-tuning" />
+						</div>
+						<div className={styles.buttonContainer}>
+							<img className={styles.buttonLabel} src={"./svg/listener-label-listen.svg"} alt="listener-label-listen" />
+							<img className={styles.button} src={"./svg/listener-button-listen.svg"} alt="listener-button-listen" />
+						</div>
+					</div>
+				</div>
+				
+				<div className={`${styles.window} ${styles.listenerVisualizer}`} id="listener-visualizer" onClick={scrollIntoThis}>
+					<WindowArrow htmlFor="listener" isLeftward={true}/>
+					<div className={styles.visualizerGrid}>
+						{visualizerItemsHtml}
+					</div>
+				</div>
+			</main>
+		</div>
+	)
 }
